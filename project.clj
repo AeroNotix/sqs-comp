df559ccc318167095a23f23afc36477d93f30519
Aaron France <aaron.l.france@gmail.com>
2015-01-05 16:10:34 +0100

    Don't exclude joda-time

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject sqs-comp "0.0.1"
+(defproject sqs-comp "0.0.2"
   :description "An SQS Client component"
   :url "http://github.com/AeroNotix/sqs-comp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[aeronotix/crap "0.4.6"]
-                 [com.cemerick/bandalore "0.0.6" :exclusions [joda-time]]
+                 [com.cemerick/bandalore "0.0.6"]
                  [com.stuartsierra/component "0.2.2"]
                  [metrics-clojure "2.3.0"]
                  [net.logstash.log4j/jsonevent-layout "1.6"]
